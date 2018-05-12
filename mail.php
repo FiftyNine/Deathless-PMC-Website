f6624273db04dd16623a23675741f3920f618e69
FiftyNine <lixbart@mail.ru>
2018-05-13 01:06:58 +0700

    Contacts page.

---
+++ mail.php
@@ -0,0 +1,29 @@
+<?php
+$to = 'damupawe@b2bx.net'; // test mail
+    $name = $_POST["name"];
+    $email= $_POST["email"];
+    $subject = $_POST["subject"];
+    $text= $_POST["message"];
+    
+
+
+    //$headers = 'MIME-Version: 1.0' . "\r\n";
+    $headers = "From: " . $email . "\r\n"; // Sender's E-mail
+    //$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+    $message ='<table style="width:100%">
+        <tr>
+            <td>'.$name.'</td>
+        </tr>
+        <tr><td>Email: '.$email.'</td></tr>
+        <tr><td>Text: '.$text.'</td></tr>
+        
+    </table>';
+    echo $headers;
+    if (mail($to, $subject, $message, $headers))
+    {
+        echo 'The message has been sent.';
+    }else{
+        echo 'failed';
+    }
+?>
